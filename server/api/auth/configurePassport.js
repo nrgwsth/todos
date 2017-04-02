e5e8d54efa3973be49db17986818d1831894a9e7
anuragawasthi13 <anuragawasthi13@gmail.com>
2017-04-02 20:39:39 +0530

    added facebook login

--- server/api/auth/configurePassport.js
+++ server/api/auth/configurePassport.js
@@ -1,16 +1,17 @@
 "use strict";
 
 const TwitterStrategy = require("passport-twitter").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const User = require("./../../models/User");
 
-module.exports = function(passport){
+module.exports = function(passport) {
 	passport.use(new TwitterStrategy({
-		consumerKey: process.env.TWITTER_CONSUMER_KEY,
-		consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-		callbackURL: process.env.Callback_Url
-	},
-	function(token, tokenSecret, profile, cb) {
-			
+			consumerKey: process.env.TWITTER_CONSUMER_KEY,
+			consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+			callbackURL: process.env.Callback_Url
+		},
+		function(token, tokenSecret, profile, cb) {
+
 			User.findOrCreate({
 				id: profile.id,
 				profile: profile
@@ -22,15 +23,31 @@ module.exports = function(passport){
 		}
 	));
 
+	passport.use(new FacebookStrategy({
+			clientID: process.env.FACEBOOK_APPID,
+			clientSecret: process.env.FACEBOOK_SCERET,
+			callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+			profileFields: ['id', 'displayName', 'photos', 'email']
+		},
+		function(accessToken, refreshToken, profile, cb) {
+			User.findOrCreate({
+				id: profile.id,
+				profile: profile
+			}, function(err, user) {
+				return cb(err, user);
+			});
+		}
+	));
+
 	passport.serializeUser(function(user, done) {
 		done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-	User.findOne({
-		id: id
-	}, function(err, user) {
-		done(err, user);
 	});
-});
+
+	passport.deserializeUser(function(id, done) {
+		User.findOne({
+			id: id
+		}, function(err, user) {
+			done(err, user);
+		});
+	});
 }
--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -10,4 +10,12 @@ module.exports = function(app, passport) {
 			failureRedirect: '/login'
 		}));
 
+	app.get("/auth/facebook", passport.authenticate("facebook"));
+
+	app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+			successRedirect: "/",
+			failureRedirect: '/login'
+		}));
+
 }
